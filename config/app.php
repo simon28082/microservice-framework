7f21644fc603a08f4327496e687d03241be36f90
crcms <crcms@crcms.cn>
2018-11-13 11:45:43 +0800

    Router alias support

--- config/app.php
+++ config/app.php
@@ -212,7 +212,7 @@ return [
         'Redis' => Illuminate\Support\Facades\Redis::class,
 //        'Request' => Illuminate\Support\Facades\Request::class,
 //        'Response' => Illuminate\Support\Facades\Response::class,
-//        'Route' => Illuminate\Support\Facades\Route::class,
+        'Route' => Illuminate\Support\Facades\Route::class,
 //        'Schema' => Illuminate\Support\Facades\Schema::class,
 //        'Session' => Illuminate\Support\Facades\Session::class,
         'Storage' => Illuminate\Support\Facades\Storage::class,
