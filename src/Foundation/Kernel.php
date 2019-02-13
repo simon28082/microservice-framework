04194492664e2521d8f2a81d01ecff38c431290a
crcms <crcms@crcms.cn>
2019-02-13 11:34:13 +0800

    Kernel update

--- src/Foundation/Kernel.php
+++ src/Foundation/Kernel.php
@@ -2,11 +2,7 @@
 
 namespace CrCms\Microservice\Foundation;
 
-use CrCms\Foundation\Transporters\Contracts\DataProviderContract;
-use CrCms\Foundation\Transporters\DataProvider;
-use Exception;
-use Throwable;
-use CrCms\Microservice\Routing\Router;
+use CrCms\Microservice\Dispatching\Dispatcher;
 use Illuminate\Support\Facades\Facade;
 use CrCms\Microservice\Server\Contracts\KernelContract;
 use CrCms\Microservice\Server\Contracts\RequestContract;
@@ -14,6 +10,8 @@ use CrCms\Microservice\Server\Contracts\ResponseContract;
 use Symfony\Component\Debug\Exception\FatalThrowableError;
 use Illuminate\Contracts\Foundation\Application as ApplicationContract;
 use Illuminate\Contracts\Debug\ExceptionHandler as ExceptionHandlerContract;
+use Exception;
+use Throwable;
 
 /**
  * Class Kernel.
@@ -24,6 +22,11 @@ class Kernel implements KernelContract
      * @var ApplicationContract
      */
     protected $app;
+
+    /**
+     * @var Dispatcher
+     */
+    protected $caller;
 
     /**
      * @var array
@@ -42,44 +45,38 @@ class Kernel implements KernelContract
      *
      * @var array
      */
-    protected $requestMiddleware = [
+    protected $middleware = [
         \CrCms\Microservice\Foundation\Middleware\CheckForMaintenanceModeMiddleware::class,
     ];
 
     /**
-     * The application data transport middleware
-     *
-     * @var array
-     */
-    protected $transportMiddleware = [
-        \CrCms\Microservice\Server\Middleware\DataEncryptDecryptMiddleware::class,
-    ];
-
-    /**
-     * Create a new HTTP kernel instance.
-     *
      * @param ApplicationContract $app
-     * @param Router $router
-     *
-     * @return void
+     * @param Dispatcher $caller
      */
-    public function __construct(ApplicationContract $app)
+    public function __construct(ApplicationContract $app, Dispatcher $caller)
     {
         $this->app = $app;
+        $this->caller = $caller;
     }
 
-    public function request(RequestContract $request)
+    /**
+     * handle
+     *
+     * @param RequestContract $request
+     * @return ResponseContract
+     */
+    public function handle(RequestContract $request): ResponseContract
     {
-        $this->app->instance('request', $request);
+        $this->app->instance('request', $this->bindRequestMatcher($request));
 
         Facade::clearResolvedInstance('request');
 
         try {
-            $response = (new \Illuminate\Pipeline\Pipeline($this->app))
+            $response = (new Pipeline($this->app))
                 ->send($request)
-                ->through($this->requestMiddleware)
-                ->then(function ($request) {
-                    return $request;
+                ->through(array_merge($this->middleware, $request->matcher()->getCallerMiddleware()))
+                ->then(function (RequestContract $request) {
+                    return $this->app->call($request->matcher()->getCallerUses());
                 });
         } catch (Exception $e) {
             $this->reportException($e);
@@ -92,29 +89,20 @@ class Kernel implements KernelContract
         return $response;
     }
 
-    public function transport(string $data, ?RequestContract $request = null): ResponseContract
+    /**
+     * bindRequestMatcher
+     *
+     * @param RequestContract $request
+     * @return RequestContract
+     */
+    protected function bindRequestMatcher(RequestContract $request): RequestContract
     {
-        try {
-            $data = $this->app->make('server.packer')->unpack($data);
+        $data = $this->app->make('server.packer')->unpack($request->rawData());
 
-            $this->app->instance('data.provider', new DataProvider(
-                array_merge($data['data'],['_request' => $request])
-            ));
-
-            Facade::clearResolvedInstance('data.provider');
-
-            $response = $this->app->make('caller.match')->match($data['call'], $this->app->get('data.provider'));
-
-            dd($response);
-        } catch (Exception $e) {
-            $this->reportException($e);
-            $response = $this->renderException(null, $e);
-        } catch (Throwable $e) {
-            $this->reportException($e = new FatalThrowableError($e));
-            $response = $this->renderException(null, $e);
-        }
-
-        return $response;
+        return $request->setData($data['data'] ?? [])
+            ->setMatcher(
+                $this->caller->getCaller($data['call'])->setContainer($this->app)
+            );
     }
 
     /**
@@ -138,28 +126,31 @@ class Kernel implements KernelContract
     }
 
     /**
-     * @param RequestContract|DataProviderContract $data
-     * @param ResponseContract $response
+     * terminate
      *
+     * @param RequestContract $request
+     * @param ResponseContract $response
      * @return mixed|void
      */
-    public function terminate($data, ResponseContract $response)
+    public function terminate(RequestContract $request, ResponseContract $response)
     {
-        $this->terminateMiddleware($data, $response);
+        $this->terminateMiddleware($request, $response);
 
         $this->app->terminate();
     }
 
     /**
-     * @param RequestContract|DataProviderContract $request
+     * terminateMiddleware
+     *
+     * @param RequestContract $request
      * @param ResponseContract $response
+     * @return void
      */
-    protected function terminateMiddleware($data, ResponseContract $response)
+    protected function terminateMiddleware(RequestContract $request, ResponseContract $response)
     {
         $middlewares = array_merge(
-            $this->requestMiddleware,
-            $this->transportMiddleware,
-            $this->app->make('caller.match')->getCallerMiddleware()
+            $this->middleware,
+            $request->matcher()->getCallerMiddleware()
         );
 
         foreach ($middlewares as $middleware) {
@@ -172,7 +163,7 @@ class Kernel implements KernelContract
             $instance = $this->app->make($name);
 
             if (method_exists($instance, 'terminate')) {
-                $instance->terminate($data, $response);
+                $instance->terminate($request, $response);
             }
         }
     }
