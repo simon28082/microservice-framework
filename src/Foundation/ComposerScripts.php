9974b5a97331841439c80c91212f8813be2d4649
crcms <crcms@crcms.cn>
2019-03-02 16:46:17 +0800

    fix: dump-autoload application error

---
+++ src/Foundation/ComposerScripts.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace CrCms\Microservice\Foundation;
+
+use Illuminate\Foundation\ComposerScripts as BaseComposerScripts;
+
+class ComposerScripts extends BaseComposerScripts
+{
+    /**
+     * Clear the cached Laravel bootstrapping files.
+     *
+     * @return void
+     */
+    protected static function clearCompiled()
+    {
+        $laravel = new Application(getcwd());
+
+        if (file_exists($servicesPath = $laravel->getCachedServicesPath())) {
+            @unlink($servicesPath);
+        }
+
+        if (file_exists($packagesPath = $laravel->getCachedPackagesPath())) {
+            @unlink($packagesPath);
+        }
+    }
+}
