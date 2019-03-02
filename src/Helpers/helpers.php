ca52fabdf420025e40dbab2185266721c662d6f6
crcms <crcms@crcms.cn>
2019-03-02 19:04:34 +0800

    perf: remove public_path function

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -268,19 +268,6 @@ if (! function_exists('now')) {
     }
 }
 
-if (! function_exists('public_path')) {
-    /**
-     * Get the path to the public folder.
-     *
-     * @param  string  $path
-     * @return string
-     */
-    function public_path($path = '')
-    {
-        return app()->make('path.public').($path ? DIRECTORY_SEPARATOR.ltrim($path, DIRECTORY_SEPARATOR) : $path);
-    }
-}
-
 if (! function_exists('report')) {
     /**
      * Report an exception.
