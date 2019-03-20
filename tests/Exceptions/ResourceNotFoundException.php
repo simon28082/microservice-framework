c14f3bfeb2ef29ed9c42752d3c296ca428203a5b
crcms <crcms@crcms.cn>
2019-03-21 07:37:22 +0800

    test: ExceptionHandler testing

---
+++ tests/Exceptions/ResourceNotFoundException.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author simon <crcms@crcms.cn>
+ * @datetime 2019-03-21 07:35
+ *
+ * @link http://crcms.cn/
+ *
+ * @copyright Copyright &copy; 2019 Rights Reserved CRCMS
+ */
+
+namespace CrCms\Microservice\Tests\Exceptions;
+
+class ResourceNotFoundException extends \Exception
+{
+
+}
